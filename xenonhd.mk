036f352b5d72c321e13d104fdc5d576ebdeca256
uppon2 <uppon2@users.noreply.github.com>
2015-04-11 19:26:23 +0930

    Update xenonhd.mk

--- xenonhd.mk
+++ xenonhd.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_find7
+PRODUCT_NAME := xenonhd_find7
 PRODUCT_DEVICE := find7
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
